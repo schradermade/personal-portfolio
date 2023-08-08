8a9eab6a4ba99e1376107e6a46732838da8ed3d5
Nathan Schrader <natecschrader@gmail.com>
2023-08-07 23:17:31 -0700

    css changes to Project

--- app/components/Project.tsx
+++ app/components/Project.tsx
@@ -17,14 +17,12 @@ const Project: React.FC<Props> = ({ siteUrl, title, imgPath, description }) => {
       className="bg-slate-700 border-2 border-slate-600 hover:bg-slate-600 hover:border-slate-500 rounded-md p-6 flex flex-col m-8 w-4/5 shadow-xl"
     >
       <h1 className="text-lime-300 text-2xl pl-1">{title}</h1>
-      <p className="text-gray-50 text-xs pl-1 pb-2">
+      <p className="text-gray-50 text-xs pl-1 pb-4">
         (click to visit live site)
       </p>
-      <h2 className="text-gray-50 pb-1 pl-1 font-semibold">
-        Project Description:
-      </h2>
+      <h2 className="text-gray-50 pl-1">DESCRIPTION:</h2>
       <div className="w-full">
-        <h3 className="text-gray-50 pl-1 pb-6">{description}</h3>
+        <h3 className="text-gray-50 pl-1 pb-4">{description}</h3>
       </div>
       <div className="max-w-xl">
         <Image
